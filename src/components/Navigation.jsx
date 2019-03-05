621e1f831de399e599e567a62e3bf1d464fb7b39
JA <joshappeldev@gmail.com>
2019-03-04 21:35:42 -0500

    removed constructor

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,19 +10,15 @@ import {
 } from "reactstrap";
 
 class Navigation extends Component {
-  constructor(props) {
-    super(props);
+  state = {
+    isOpen: false
+  };
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
+  toggle = () => {
     this.setState({
       isOpen: !this.state.isOpen
     });
-  }
+  };
   render() {
     return (
       <div className="sticky-top">
