da6653e0724fd3e257fd4e0dfc299838f12ecebc
JA <joshappeldev@gmail.com>
2019-03-06 22:47:44 -0500

    animation crazy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,26 @@
 import React from "react";
+import { Animated } from "react-animated-css";
 
 const Footer = () => {
   return (
     <React.Fragment>
-      <div className="spacer mt-4" />
       <footer className="text-center sticky-footer">
-        <a
-          href="https://github.com/jhadev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="text-light fab fa-github icon-large" />
-        </a>
-        <a
-          href="http://linkedin.com/in/josh-a-9865bb175"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="text-light mx-1 fab fa-linkedin-in icon-large" />
-        </a>
+        <Animated animationIn="slideInUp">
+          <a
+            href="https://github.com/jhadev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="text-light fab fa-github icon-large" />
+          </a>
+          <a
+            href="http://linkedin.com/in/josh-a-9865bb175"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="text-light mx-1 fab fa-linkedin-in icon-large" />
+          </a>
+        </Animated>
       </footer>
     </React.Fragment>
   );
