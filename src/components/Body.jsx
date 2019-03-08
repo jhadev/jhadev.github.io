61f3de64d4ab2c79737f40c0ac6ae9fb01bc65b5
JA <joshappeldev@gmail.com>
2019-03-08 18:04:36 -0500

    navbar links go to correct div

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -15,47 +15,51 @@ class Body extends Component {
     return (
       <Container>
         <Animated animationIn="zoomIn">
-          <Row className="justify-content-center mt-5">
-            <Col md="6" xs="12">
-              <h2 id="about" className="about text-center text-light mb-3">
-                About Me
-              </h2>
-              <About />
-            </Col>
-          </Row>
-          <Row className="mt-5">
-            <Col xs="12">
-              <h2
-                id="portfolio"
-                className=" portfolio text-center text-light mb-3"
-              >
-                Portfolio
-              </h2>
-            </Col>
-          </Row>
-          <Row>
-            {this.state.projects.map(project => (
-              <ProjectCard
-                key={project.id}
-                id={project.id}
-                name={project.name}
-                image={project.image}
-                github={project.github}
-                description={project.description}
-                link={project.link}
-              />
-            ))}
-          </Row>
-          <Row className="justify-content-center mt-5">
-            <Col md="6" xs="12">
-              <h2 id="contact" className="contact text-center text-light mb-3 ">
-                Contact
-              </h2>
-            </Col>
-          </Row>
-          <Row className="justify-content-center">
-            <ContactForm />
-          </Row>
+          <span className="anchor" id="about" />
+          <section id="about-section">
+            <Row className="justify-content-center mt-5">
+              <Col md="6" xs="12">
+                <h2 className="about text-center text-light mb-3">About Me</h2>
+                <About />
+              </Col>
+            </Row>
+          </section>
+          <span className="anchor" id="portfolio" />
+          <section id="portfolio-section">
+            <Row className="mt-5">
+              <Col xs="12">
+                <h2 className=" portfolio text-center text-light mb-3">
+                  Portfolio
+                </h2>
+              </Col>
+            </Row>
+            <Row>
+              {this.state.projects.map(project => (
+                <ProjectCard
+                  key={project.id}
+                  id={project.id}
+                  name={project.name}
+                  image={project.image}
+                  github={project.github}
+                  description={project.description}
+                  link={project.link}
+                />
+              ))}
+            </Row>
+          </section>
+          <span className="anchor" id="contact" />
+          <section id="contact-section">
+            <Row className="justify-content-center mt-5">
+              <Col md="6" xs="12">
+                <h2 className="contact text-center text-light mb-3 ">
+                  Contact
+                </h2>
+              </Col>
+            </Row>
+            <Row className="justify-content-center">
+              <ContactForm />
+            </Row>
+          </section>
         </Animated>
       </Container>
     );
